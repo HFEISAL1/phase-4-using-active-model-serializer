5accb8302c08ffa605cd66c0a6bc62100ad33291
hfeisal1 <halima.feisal@student.moringaschool.com>
2022-10-07 13:07:18 -0700

    changes made on config routes.rb

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,10 @@
 Rails.application.routes.draw do
   resources :movies, only: [:index, :show]
-  get '/movies/:id/summary', to: 'movies#summary'
+  get '/movie_summaries', to: 'movies#summaries'
+
+# app/controllers/movies_controller.rb
+def summaries
+  movies = Movie.all
+  render json: movies, each_serializer: MovieSummarySerializer
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
